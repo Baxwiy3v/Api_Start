724af632199aaaaee5405f9f6050bef5741cfd9a
Baxwiy3v_ <tu7nxadgf@code.edu.az>
2023-12-13 23:12:00 +0400

    Tag And Category Controller added

---
+++ App104/Controllers/TagsController.cs
@@ -0,0 +1,94 @@
+﻿using App104.DAl;
+using App104.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App104.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TagsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get(int page = 1, int take = 2)
+        {
+            List<Tag> Tags = await _context.Tags
+                .Skip((page - 1) * take)
+                .Take(take)
+                .ToListAsync();
+
+            return Ok(Tags);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0) return StatusCode(StatusCodes.Status400BadRequest);
+            Tag tag = await _context.Tags
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (tag is null) return StatusCode(StatusCodes.Status404NotFound);
+            return StatusCode(StatusCodes.Status200OK,tag);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            await _context.Tags.AddAsync(tag);
+
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+            
+            if (id <= 0) return StatusCode(StatusCodes.Status400BadRequest);
+           
+            Tag tag = await _context.Tags
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (tag is null) return StatusCode(StatusCodes.Status404NotFound);
+            tag.Name = name;
+
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status204NoContent,tag);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            
+            if (id <= 0) return StatusCode(StatusCodes.Status400BadRequest);
+            
+            Tag tag = await _context.Tags
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (tag is null) return StatusCode(StatusCodes.Status404NotFound);
+          
+            _context.Tags.Remove(tag);
+           
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status204NoContent,tag);
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ App104/DAl/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using App104.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App104.DAl
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options) { }
+       
+
+       public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+
+
+
+    }
+}
